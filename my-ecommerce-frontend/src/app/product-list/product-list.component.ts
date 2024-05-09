d88ded62497cdc927147b8882e54cfda2b55d486
PercyKomane <percykomane13@gmail.com>
2024-05-09 19:00:07 +0200

    added some basic html

--- my-ecommerce-frontend/src/app/product-list/product-list.component.ts
+++ my-ecommerce-frontend/src/app/product-list/product-list.component.ts
@@ -1,3 +1,5 @@
+// product-list.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 
@@ -7,17 +9,16 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
   products: any[] = [];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe(
-      (data) => {
-        this.products = data;
+      (response: any) => {
+        this.products = response;
       },
-      (error) => {
+      (error: any) => {
         console.error('Error fetching products:', error);
       }
     );
