6638042ce4b8f2bb05ffabdf6dfa7b6a5da52ac1
PercyKomane <percykomane13@gmail.com>
2024-05-12 22:15:15 +0200

    updates in home component

--- my-ecommerce-frontend/src/app/home/home.component.ts
+++ my-ecommerce-frontend/src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+  newProduct: any = {};
+  editingProductId: number | null = null;
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(
+      (response: any) => {
+        this.products = response;
+      },
+      (error: any) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 }
